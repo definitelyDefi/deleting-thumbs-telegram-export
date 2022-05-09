8b5ab3fda62bf3b4dd76f52043ed013d8b2b1c5f
Denis Beliy <yuichirohakya227@gmail.com>
2022-05-09 17:08:25 +0300

    some beauty chang

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ for filename in os.listdir(directory):
 
     if os.path.isfile(f) and os.path.splitext(f)[0].endswith('_thumb') or os.path.splitext(f)[0].endswith('(1)') or os.path.splitext(f)[0].endswith('(2)') or os.path.splitext(f)[0].endswith('(3)') or os.path.splitext(f)[0].endswith('(4)'):
         os.remove(f)
-        print('[DELETED] - ',os.path.basename(f))
-        
+        print('[+] Deleted - ',os.path.basename(f))
+
+print('[+] Done.')
+os.remove(os.path.join(directory,'main.py'))
+print('[+] Script deleted.')
