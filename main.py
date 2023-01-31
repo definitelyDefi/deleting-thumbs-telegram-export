d0c17b033988747a31ea4fdcb4c54ad8b1385717
Denis Beliy <yuichirohakya227@gmail.com>
2023-01-31 22:31:56 +0200

    foo

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import os
-
+import re
 folders = ['photos','stickers','files','video_files','round_video_messages']
 
 directory = os.getcwd()
 
+
+
 def delete_thumbs(directory, folder):
     for filename in os.listdir(os.path.join(directory,folder)):
         f = os.path.join(directory, folder, filename)
-        if os.path.isfile(f) and os.path.splitext(f)[0].endswith('_thumb') or \
-        os.path.splitext(f)[0].endswith('(1)') \
-        or os.path.splitext(f)[0].endswith('(2)') \
-        or os.path.splitext(f)[0].endswith('(3)') \
-        or os.path.splitext(f)[0].endswith('(4)'):
+        if os.path.isfile(f) and os.path.splitext(f)[0].endswith('_thumb') or re.search('_thumb|_thumb([1-9])',os.path.splitext(f)[0]):
             os.remove(f)
             print('[+] Deleted - ',os.path.basename(f))
 
